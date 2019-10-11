bd549cd76612eb6abe42e55b648ed6276b57ff42
Alex Wey <53589584+alex-wey@users.noreply.github.com>
2019-10-10 21:12:18 -0400

    Add files via upload

---
+++ common.h
@@ -0,0 +1,36 @@
+/*
+ * Enum to represent the four directions
+ * Here is an example of how to use an enum:
+ *
+ * Direction dir = EAST;
+ * if (dir == EAST) {
+ *     printf("Heading EAST!");
+ * }
+ */
+typedef enum { NORTH = 0, SOUTH = 1, WEST = 2, EAST = 3 } Direction;
+
+/*
+ * Struct to represent a room in the maze.
+ * Must store:
+ * 	- The location of the room
+ * 	- Whether or not this room has been visited
+ *	- The connections to neighboring rooms in all
+ *	  four directions.
+ *  - FOR SOLVER ONLY: A pointer to the next room, set this pointer
+ *    in the current room's struct to the next room you visit
+ */
+struct maze_room {
+    int row, col;
+    int visited;
+    int con[4];
+    struct maze_room *next;
+};
+
+int is_in_range(int row, int col, int num_rows, int num_cols);
+
+struct maze_room *get_neighbor(int num_rows, int num_cols,
+                               struct maze_room maze[][num_cols],
+                               struct maze_room *room, Direction dir);
+
+void initialize_maze(int num_rows, int num_cols,
+                     struct maze_room maze[num_rows][num_cols]);
